ab9e469c18dc1166a22c22880a89923a56d125e3
Bartbabart <Bart.de.vries@outlook.com>
2022-06-03 18:53:12 +0200

    kleine update op vorige push
    iets meer structuur toegevoegd

--- Op3-Paxos-implementatie/Computers/Acceptor.py
+++ Op3-Paxos-implementatie/Computers/Acceptor.py
@@ -7,10 +7,15 @@ class Acceptor(Computer):
         self.propose_id = 0
 
     def take_in_prepare(self, message):
-        # als pc_id groter is dan propose_id maak propose_id pc_id en return promise message.
-        return 0
+        if self.propose_id < # message source id
+            self.propose_id = # source_id van message
+            return 0# promise message
 
     def take_in_accept(self, message):
         # als pc_id gelijk is aan propose_id maak de gegeven value self.value en geef en return een accepted message,
         # anders return een rejected message.
-        return 0
+        if self.propose_id == :# message source_id
+            self.value = # message value
+            return 0# accepted message
+        else:
+            return 0# rejected message
--- Op3-Paxos-implementatie/Computers/Proposer.py
+++ Op3-Paxos-implementatie/Computers/Proposer.py
@@ -2,12 +2,15 @@ from Computers import Computer
 
 
 class Propose(Computer):
+
     propose_id = 0
 
     def __init__(self, pc_id, value=None):
         super().__init__(pc_id, value)
         self.propose_id = None
         self.suggest_value = None
+        self.agreement = False
+        self.accepted = 0
 
     def take_in_propose(self, value):
         self.value = value
@@ -15,7 +18,18 @@ class Propose(Computer):
         self.propose_id = Propose.propose_id
 
     def take_in_promise(self, message):
-        return 0
+        if # message een value heeft:
+            self.value = # value van de source message
 
-    def take_in_accepted(self, message):
-        return 0
+        return 0 # naar alle acceptors een accept message sturen
+
+    def take_in_accepted(self, message, majority):
+        self.accepted += 1
+        if self.accepted >= majority and not self.agreement:
+            self.agreement = True
+
+
+    def reset(self):
+        self.accepted = 0
+        Propose.propose_id += 1
+        self.propose_id = Propose.propose_id
